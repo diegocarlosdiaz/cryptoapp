63438fbd4fb3fc88697a0af6975ea0058ec8d64f
diegocarlosdiaz <diegocarlosdiaz5@gmail.com>
2022-08-09 03:04:12 -0300

    arreglo del enrutador

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
+import 'antd/dist/antd.min.css';
 
 ReactDOM.render(
     <Router>
         <App />
-    </Router>, document.getElementById('root'));
+    </Router>, 
+    document.getElementById('root')
+    );
